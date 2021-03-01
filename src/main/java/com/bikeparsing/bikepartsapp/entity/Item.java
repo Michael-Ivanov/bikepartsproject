b9feec3131ecba1a07e54f8d13fd60db76f4e629
Michael Ivanov <eesseth@gmail.com>
2021-03-01 21:46:25 +0300

    Update item class

--- src/main/java/com/bikeparsing/bikepartsapp/entity/Item.java
+++ src/main/java/com/bikeparsing/bikepartsapp/entity/Item.java
@@ -6,5 +6,65 @@ public class Item {
 
     private String name;
 
+    private String price;
 
+    private String availability;
+
+    public Item() {
+    }
+
+    public Item(String name, String price, String availability) {
+        this.name = name;
+        this.price = price;
+        this.availability = availability;
+    }
+
+    public Item(int id, String name, String price, String availability) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.availability = availability;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price='" + price + '\'' +
+                ", availability='" + availability + '\'' +
+                '}';
+    }
 }
